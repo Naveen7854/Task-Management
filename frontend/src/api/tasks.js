770f86e4a634c496a2a4409e2a4acbb0570a72e5
Naveen7854 <banothnaveen753@gmail.com>
2024-12-30 05:16:10 +0530

    Backend Deployed

--- frontend/src/api/tasks.js
+++ frontend/src/api/tasks.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/tasks";
+const API_URL = "https://task-management-t5p6.onrender.com/tasks";
 
 export const fetchTasks = async () => {
   const response = await axios.get(API_URL);
